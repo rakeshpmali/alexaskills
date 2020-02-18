780625df2fcf97cc436cb8afe3d3d3d3124829e6
Rakesh Mali <rakeshmali.in@gmail.com>
2020-02-18 10:47:12 +0530

    Final Collection

---
+++ aws-iot-wavelexa/startWaveLexaServer.sh
@@ -0,0 +1,1 @@
+node aws-iot-wavelexa.js -k "/home/rocky/aws-iot/certs/optimus-raspi-private.pem.key" -c "/home/rocky/aws-iot/certs/optimus-raspi-certificate.pem.crt" -i "optimus-raspi-instance-1" -a "/home/rocky/aws-iot/certs/AmazonRootCA1.pem" -H "a2r4xut67h09tk-ats.iot.ap-south-1.amazonaws.com" -p 8883 -T "optimus-raspi" --test-mode 1
